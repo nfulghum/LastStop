4962480bd686c498f89bff6c8252bdede010c3c3
Nick Fulghum <nickfulghum92@gmail.com>
2022-09-22 08:19:05 -0500

    updated seed file

--- seed.py
+++ seed.py
@@ -1,17 +1,5 @@
-
-
 from app import app
 from models import db, User
 
 db.drop_all()
 db.create_all()
-
-u1 = User(
-    name="Nick",
-    email="nick@gmail.com",
-    phone=222-222-2222,
-    username="CampingGuy",
-    city="Houston",
-    state="TX",
-    zip=77055
-)
